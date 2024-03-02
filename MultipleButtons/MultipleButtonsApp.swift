5f36b361ffa720c2513586e87d1912fbbcfc6aaf
Sandy Gomez <sgomez1029@gmail.com>
2024-03-01 17:23:04 -0800

    Initial Commit

---
+++ MultipleButtons/MultipleButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleButtonsApp.swift
+//  MultipleButtons
+//
+//  Created by Sandra Gomez on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MultipleButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
